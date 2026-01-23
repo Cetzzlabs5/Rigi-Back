0de707367932105fbde04d64b4a2dd42ae16800d
Facundo Zuleta <facundozuleta70@gmail.com>
2026-01-23 16:10:07 -0300

    fix provider

--- src/routes/providerActivityRoutes.ts
+++ src/routes/providerActivityRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import { ProviderActivityController } from '../controllers/ProviderActivityController.js'
+import { authenticate } from '../middleware/auth.js';
 
 const router = Router()
 
-router.get('/', ProviderActivityController.getProviderActivities)
+router.get('/', authenticate, ProviderActivityController.getProviderActivities)
 
 export default router as Router
--- src/routes/providerRoutes.ts
+++ src/routes/providerRoutes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express'
 import { ProviderController } from '../controllers/ProviderController.js'
+import { authenticate } from '../middleware/auth.js';
+import { handleInputErrors } from '../middleware/validation.js';
+import { body } from 'express-validator';
 
 const router = Router()
 
-router.get('/:id', ProviderController.getProfile)
-router.patch('/:id', ProviderController.updateProfile)
+router.get('/:id', authenticate, ProviderController.getProfile)
+router.patch('/:id', authenticate,
+    body('phone').isLength({ min: 8 }).isNumeric().withMessage('El teléfono es requerido'),
+    body('providerActivityId').notEmpty().withMessage('La actividad es requerida'),
+    body('address').notEmpty().withMessage('La dirección es requerida'),
+    body('province').notEmpty().withMessage('La provincia es requerida'),
+    handleInputErrors, ProviderController.updateProfile)
 
 export default router as Router
